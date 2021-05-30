f782fcd12865c2cc6b9dba1da255c138a4fae789
satanhimself-666 <84249581+satanhimself-666@users.noreply.github.com>
2021-05-30 12:11:38 +0530

    Add files via upload

---
+++ odd or even.py
@@ -0,0 +1,50 @@
+
+print("                            hello")
+
+
+
+print(":::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::")
+
+
+print("..........................\
+\
+\
+\
+\
+\
+\
+..........")
+
+print("--------welcome to odd or even game----------")
+
+
+
+
+
+
+
+
+
+
+number=int(input("======enter a whole number======"))
+
+
+
+
+
+
+b=number%2
+
+if b==1:
+   print("it is an odd number")
+   
+print("°°°°°°°°°°°°°°°°°°°°°°°°°°")
+
+if b==0:
+  print("it is an even number")
+
+print("✓✓✓✓✓✓✓✓✓✓✓✓✓✓✓✓✓✓✓✓✓✓")  
+  
+    
+
+
